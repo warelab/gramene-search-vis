0331443efb30462e770ba0654d0878fa7de1d116
mycrobe <jmulvane@cshl.edu>
2015-06-17 16:02:28 -0400

    do a real search

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 var d3 = require('d3');
 var taxonomyGetter = require('gramene-taxonomy-with-genomes');
+var search = require('gramene-search-client').client;
+var Q = require('q');
+
+// Example query object.
+// This is usually generated in code by gramoogle.
+// This query:
+//   1. asks for all genes with a particular InterPro domain
+//   2. requests binned results that will match the bin configuration
+//      of the taxonomy object
+//   3. requests also to get taxonomy facet.
+var exampleQuery = {
+  "q": "",
+  "filters": {
+    "interpro_ancestors:2347": {
+      "fq": "interpro_ancestors:2347"
+    }
+  },
+  "resultTypes": {
+    "taxon_id": {"facet.field": "{!facet.limit='50' facet.mincount='0' key='taxon_id'}taxon_id"},
+    "fixed_200_bin": {"facet.field": "{!facet.limit='-1' facet.mincount='1' key='fixed_200_bin'}fixed_200_bin"}
+  }
+};
 
 var height = 2000, width = 960;
 
@@ -15,7 +37,13 @@ var svg = d3.select("body").append("svg")
   .attr("height", height)
   .append("g");
 
-taxonomyGetter.get().then(function (taxonomy) {
+Q.all([
+  taxonomyGetter.get(true), // FOR NOW, use local data
+  search.geneSearch(exampleQuery)
+]).spread(function (taxonomy, results) {
+  taxonomy.setBinType('fixed', 200);
+  taxonomy.setResults(results.fixed_200_bin);
+
   var nodes = tree.nodes(taxonomy),
     links = tree.links(nodes);
 
@@ -39,6 +67,8 @@ taxonomyGetter.get().then(function (taxonomy) {
     .attr("dx", ".6em")
     .attr("text-anchor", "start")
     .text(function (d) { return d.model.name; });
+}).catch(function (err) {
+  console.error(err);
 });
 
 d3.select(self.frameElement).style("height", height + "px");
