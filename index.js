a5cbc3a66cda01c08632ea9295c1530a7dd7c750
Jim Thomason <jim@jimandkoka.com>
2015-06-30 16:20:07 -0500

    new code

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var taxonomyGetter = require('gramene-taxonomy-with-genomes');
 var search = require('gramene-search-client').client;
 var Q = require('q');
 
+var $ = require('jquery');
+
+require('./kb/WareTreeGeneDistribution.js');
+
 // Example query object.
 // This is usually generated in code by gramoogle.
 // This query:
@@ -23,61 +27,28 @@ var exampleQuery = {
   }
 };
 
-var height = 2000, width = 960;
+//var height = 2000, width = 960;
+var width = 960;
 
-var tree = d3.layout.tree()
-  .size([height, width - 160])
-  .separation(function (a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+   var $div = $.jqElem('div')
+        .css({width : width})
+    ;
 
-var diagonal = d3.svg.diagonal()
-  .projection(function (d) { return [d.y, d.x]; });
+   $('body').append($div);
 
-var svg = d3.select("body").append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .append("g");
+    Q.all([
+      taxonomyGetter.get(), // FOR NOW, use local data
+      search.geneSearch(exampleQuery)
+    ]).spread(function (taxonomy, results) {
+      taxonomy.setBinType('fixed', 200);
+      taxonomy.setResults(results.fixed_200_bin);
 
-Q.all([
-  taxonomyGetter.get(), // FOR NOW, use local data
-  search.geneSearch(exampleQuery)
-]).spread(function (taxonomy, results) {
-  taxonomy.setBinType('fixed', 200);
-  taxonomy.setResults(results.fixed_200_bin);
+taxonomy = taxonomy.children[0];
+delete taxonomy.parent;
 
-  var nodes = tree.nodes(taxonomy),
-    links = tree.links(nodes);
+$div.WareTreeGeneDistribution({ dataset : taxonomy });
 
-  var link = svg.selectAll(".link")
-    .data(links)
-    .enter().append("path")
-    .attr("class", "link")
-    .attr("d", diagonal);
-
-  var node = svg.selectAll(".node")
-    .data(nodes)
-    .enter().append("g")
-    .attr("class", "node")
-    .attr("transform", function (d) { return "translate(" + d.y + ", " + d.x + ")"; })
-    .on("mouseover", function(d) { console.log(d) });
-
-  node.append("circle")
-    .attr("r", 4.5);
-
-  node.append("text")
-    .attr("dy", ".31em")
-    .attr("dx", ".6em")
-    .attr("text-anchor", "start")
-    .text(function (d) {
-      var name = d.model.name;
-      if(d.model.genome) {
-        name += ' (' + d.model.genome.results.count +
-          ' results in ' + d.model.genome.results.bins +
-          ' bins)';
-      }
-      return name;
-    });
+;
 }).catch(function (err) {
   console.error(err);
 });
-
-d3.select(self.frameElement).style("height", height + "px");
---
+++ kb/WareTreeGeneDistribution.js
@@ -0,0 +1,191 @@
+/*
+
+*/
+
+var $ = require('jquery');
+require('./kbaseTreechart.js');
+require('./GeneDistribution.js');
+
+    $.KBWidget({
+
+	    name: "WareTreeGeneDistribution",
+
+        version: "1.0.0",
+        options: {},
+
+        init : function(options) {
+
+            this._super(options);
+
+            var relayout = function(d) {
+
+                delete d.depth;
+                delete d.id;
+                delete d.x;
+                delete d.x0;
+                delete d.y;
+                delete d.y0;
+                delete d.parent;
+                delete d.stroke;
+
+                if (d.children) {
+                    $.each(
+                        d.children,
+                        function (idx, kid) {
+                            relayout(kid);
+                        }
+                    )
+                }
+
+                return d;
+            }
+
+            this.$elem.kbaseTreechart(
+                {
+
+                    nodeEnterCallback : function(d, i, node, duration) {
+                        if (d.model.genome) {
+                            var $tree = this;
+
+                            var bounds = this.chartBounds();
+
+                            var y = $tree.options.fixed && (! d.children || d.length == 0)
+                                ? $tree.options.fixedDepth
+                                : d.y;
+
+                            var width = bounds.size.width - y - this.options.labelWidth - 10;
+
+
+                            var lgvSelection = d3.select(node).selectAll('.lgv').data([d]);
+
+                            var lgvID = 'lgv-' + $tree.uuid();
+                            var nodeHeight = node.getBBox().height;
+
+                            if ($tree.options.lgvTransform == undefined) {
+                                $tree.options.lgvTransform =
+                                    'translate(' + ($tree.options.labelWidth + 10) + ',' + (0 - nodeHeight / 2) + ') , ' +
+                                    'scale(' + width / bounds.size.width + ',' + nodeHeight / $tree.$elem.height() + ')'
+                            }
+
+
+                            lgvSelection
+                                .enter()
+                                    .append('g')
+                                        .attr('class', lgvID)
+                                        .attr('transform', $tree.options.lgvTransform)
+                            ;
+
+                            if (d.$lgv == undefined) {
+                                d.$lgv = $.jqElem('div').GeneDistribution(
+                                    {
+                                        scaleAxes   : true,
+                                        customRegions : {
+                                            chart : lgvID
+                                        },
+                                        parent : $tree,
+                                    }
+                                );
+                            }
+
+                            d.$lgv.options.customRegions.chart = lgvID;
+                            d.$lgv.setDataset(d.model.genome);
+
+                        }
+                    },
+
+                    nodeUpdateCallback : function(d,i,node, duration) {
+                        if (! d.children || ! d.children.length) {
+                            d3.select(node).selectAll('.lgv').data([d]).transition().duration(duration).attr('opacity', 1);
+                        }
+                    },
+
+                    nodeExitCallback : function(d,i,node, duration) {
+                        if (! d.children || ! d.children.length) {
+                            d3.select(node).selectAll('.lgv').data([d]).exit().transition().duration(duration).attr('opacity', 0);
+                        }
+                    },
+
+
+                    dataset : this.options.dataset,
+                    displayStyle : 'Nnt',
+                    lineStyle : 'square',
+                    layout : 'cluster',
+                    distance : 10,
+                    fixed : true,
+                    labelWidth : 250,
+                    nameFunction : function (d) {
+                      var name = d.model.name;
+                      if(d.model.genome) {
+                        name += ' (' + d.model.genome.results.count +
+                          ' results in ' + d.model.genome.results.bins +
+                          ' bins)';
+                      }
+                      return name;
+                    },
+                    truncationFunction : function(d, elem, $tree) {
+                        d3.select(elem)
+                        .on('mouseover', function(d) {
+                            $tree.showToolTip({label : d.name});
+                        })
+                        .on('mouseout', function(d) {
+                            $tree.hideToolTip();
+                        });
+                        return d.name_truncated + '...';
+                    },
+                    nodeDblClick : function(d) {
+                        this.options.textDblClick.call(this, d);
+                    },
+                    textClick : function(d) {
+
+                        var max = Math.floor(Math.random() * 10);
+
+                        var dataset = [];
+                        for (var i = 0; i <= max; i++) {
+                            dataset.push(
+                                {
+                                    start : i,
+                                    end : i + 1,
+                                    score : Math.random() * 100
+                                }
+                            )
+                        }
+
+                        $gd.setDataset(dataset);
+                    },
+                    textDblClick : function(d) {
+                        var parent;
+                        if (this.filterParent == undefined) {
+                            this.filterParent = [];
+                        }
+
+                        if (! d.parent && this.filterParent.length) {
+                            d = this.filterParent.pop();
+                            delete d.stroke;
+                        }
+                        else {
+                            var parent = d.parent;
+                            while (parent != undefined) {
+                                this.filterParent.unshift(parent);
+                                parent = parent.parent;
+                            }
+                        }
+
+                        if (d.children && d.children.length) {
+                            relayout(d);
+                            console.log("WTF ? ", d);
+                            d.stroke = this.filterParent.length ? 'cyan' : 'darkslateblue';
+                            this.setDataset(d);
+                        }
+
+                    },
+                    tooltip : function(d) {
+                        this.showToolTip({label : d.name})
+                    },
+                }
+            )
+
+            return this;
+        },
+
+
+    });
