23192f74d0067f4ab2de2b60d7bd3fb80ddff35b
mycrobe <jmulvane@cshl.edu>
2015-07-07 12:46:52 -0400

    add debounce and console log messages to vis.js

--- vis.js
+++ vis.js
@@ -3,6 +3,7 @@
 var $ = require('jquery');
 var React = require('react');
 var WareTreeGeneDistribution = require('./kb/WareTreeGeneDistribution');
+var debounce = require('lodash.debounce');
 
 var _counterForVisId = 0;
 
@@ -16,38 +17,44 @@ var Vis = React.createClass({
 
   componentWillMount: function () {
     this.visId = "vis" + _counterForVisId++;
+    this.updateVis = debounce(this.undebouncedUpdateVis, 200);
   },
 
   componentDidMount: function () {
+    console.log("Will display visualization", this.visId);
+    var reactProps = this.props;
     this.wareTreeGeneDist = WareTreeGeneDistribution.call(
       $("." + this.visId),
       {
         dataset: this.props.taxonomy.children[0],
-        taxonClick: function (d, node) {
+        taxonClick: function (d) {
           console.log("I clicked on", d, " and it is now : ", this.nodeState(d));
         },
         taxonDblClick: function (d, isRoot) {
-          console.log("III double clicked on", d, " and it is now : ", this.nodeState(d), " and is the root : ", isRoot);
+          console.log("I double clicked on", d, " and it is now : ", this.nodeState(d), " and is the root : ", isRoot);
         },
-        subtreeCollapse : function(d) {
-            console.log("collapsed under ", d);
-        },
-        subtreeExpand : function(d) {
-            console.log("expanded under ", d);
-        },
-        treeRootChange : function(d) {
-            console.log('changed root to ', d);
-        },
+        subtreeCollapse : reactProps.onSubtreeCollapse || function() {},
+        subtreeExpand : reactProps.onSubtreeExpand || function() {},
+        treeRootChange : reactProps.onTreeRootChange || function() {}
       }
     )
   },
 
   shouldComponentUpdate: function (newProps, newState) {
-    this.wareTreeGeneDist.setDataset(newProps.taxonomy.children[0]);
+    console.log("Updating visualization with new data.");
+    this.updateVis(newProps.taxonomy);
     return false;
   },
 
+  undebouncedUpdateVis: function(taxonomy) {
+    this.wareTreeGeneDist.setDataset(taxonomy.children[0]);
+  },
+
   render: function () {
+    if(this.weCalledRenderAlready) {
+      console.warn("We called Vis.render already for this instance. Why is it being called again?");
+    }
+    this.weCalledRenderAlready = true;
     return React.DOM.div(
       {className: "ware-tree-gene " + this.visId}
     );
